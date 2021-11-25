7a1f9f367a8137a58facb1d85347dbcba1614336
Marcin Soja <marcin.soja@truelayer.com>
2021-11-25 09:11:49 +0000

    Updated to use the latest TruelayerPaymentsSdk - v1.0.0 - Private Beta

--- Package.swift
+++ Package.swift
@@ -3,18 +3,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "TruelayerPayments",
+    name: "TruelayerPaymentsSdk",
     products: [
         .library(
-            name: "TruelayerPayments",
-            targets: ["TruelayerPayments"]
+            name: "TruelayerPaymentsSdk",
+            targets: ["TruelayerPaymentsSdk"]
         ),
     ],
     targets: [
         .binaryTarget(
-            name: "TruelayerPayments",
-            url: "https://github.com/TrueLayer/truelayer-ios-sdk/releases/download/0.0.1-pre1/TruelayerPayments.xcframework.zip",
-            checksum: "90785c5772ef242221bcc803152e7854fd673817a9033800daf32edeaf83c438"
+            name: "TruelayerPaymentsSdk",
+            url: "https://github.com/TrueLayer/truelayer-ios-sdk/releases/download/v1.0.0-private-beta/TruelayerPaymentsSdk.xcframework.zip",
+            checksum: "1c8d1a2b5708cbdca88575001e571f8e5bc40d78b60931cf3dd9f9ef048e96f7"
         ),
     ]
 )
