2d5b7eeb9a4ecc050466c62fa639a0c4b007e8fd
Daven Gomes <daven.gomes@truelayer.com>
2022-03-23 11:33:27 +0000

    [PUX-1410] Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,19 @@
+import PackageDescription
+
+let package = Package(
+    name: "TrueLayerPaymentsSDK",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "TrueLayerPaymentsSDK",
+            targets: ["TrueLayerPaymentsSDK"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "TrueLayerPaymentsSDK",
+            url: "https://github.com/TrueLayer/TrueLayer-iOS-SDK/releases/download/1.0.0-beta.1/TrueLayerPaymentsSDK.xcframework.zip",
+            checksum: "3e68884aed04780a2be06b26d136cb62220ed3034b0a7312fdc0b48fca42af0c"
+        ),
+    ]
+)
