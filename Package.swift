199d5d62eceddba182f5084644f538f294888c56
Daven Gomes <daven.gomes@truelayer.com>
2022-03-24 18:16:00 +0000

    [PUX-1410] Testing ignoring files

--- Package.swift
+++ Package.swift
@@ -3,19 +3,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "TrueLayerPaymentsSDK",
-    platforms: [.iOS(.v13)],
-    products: [
-        .library(
-            name: "TrueLayerPaymentsSDK",
-            targets: ["TrueLayerPaymentsSDK"]
-        ),
-    ],
-    targets: [
-        .binaryTarget(
-            name: "TrueLayerPaymentsSDK",
-            url: "https://github.com/TrueLayer/TrueLayer-iOS-SDK/releases/download/1.0.0-beta.1/TrueLayerPaymentsSDK.xcframework.zip",
-            checksum: "3e68884aed04780a2be06b26d136cb62220ed3034b0a7312fdc0b48fca42af0c"
-        ),
-    ]
+  name: "TrueLayerPaymentsSDK",
+  platforms: [.iOS(.v13)],
+  products: [
+    .library(
+      name: "TrueLayerPaymentsSDK",
+      targets: ["TrueLayerPaymentsSDK"]
+    ),
+  ],
+  targets: [
+    .binaryTarget(
+      name: "TrueLayerPaymentsSDK",
+      url: "https://github.com/TrueLayer/TrueLayer-iOS-SDK/releases/download/1.0.0-beta.1/TrueLayerPaymentsSDK.xcframework.zip",
+      checksum: "3e68884aed04780a2be06b26d136cb62220ed3034b0a7312fdc0b48fca42af0c"
+    ),
+    .target(
+      name: "TrueLayerPaymentsSDKWrapper",
+      exclude: [
+        "README.md",
+      ]
+    )
+  ]
 )
