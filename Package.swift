b99670305f86b2497d3b2aee86703dab063135c2
tl-admins <admins+github@truelayer.com>
2022-05-31 08:38:19 +0000

    chore: Bump version to 1.0.0-beta.5

--- Package.swift
+++ Package.swift
@@ -1,21 +1,23 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
-    name: "TrueLayerPaymentsSDK",
-    platforms: [.iOS(.v13)],
-    products: [
-        .library(
-            name: "TrueLayerPaymentsSDK",
-            targets: ["TrueLayerPaymentsSDK"]
-        ),
-    ],
-    targets: [
-        .binaryTarget(
-            name: "TrueLayerPaymentsSDK",
-            url: "https://github.com/TrueLayer/TrueLayer-iOS-SDK/releases/download/1.0.0-beta.5/TrueLayerPaymentsSDK.xcframework.zip",
-            checksum: "327fb58bd3e8c73ebfa60e5de6d21b3a91103ee396ca04997443d2017d0ce5db"
-        ),
-    ]
+  name: "TrueLayerPaymentsSDK",
+  platforms: [
+    .iOS(.v10)
+  ],
+  products: [
+    .library(
+      name: "TrueLayerPaymentsSDK",
+      targets: ["TrueLayerPaymentsSDK"]
+    ),
+  ],
+  targets: [
+    .binaryTarget(
+      name: "TrueLayerPaymentsSDK",
+      url: "https://github.com/TrueLayer/truelayer-ios-sdk/releases/download/1.0.0-beta.5/TrueLayerPaymentsSDK.xcframework.zip",
+      checksum: "d2ae77ca0f1830030643192f692bf97133727a343109e7a2b44d137791b48ac0"
+    ),
+  ]
 )
