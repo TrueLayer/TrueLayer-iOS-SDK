a57aec82154e704d1880678f9eec60732348a9fc
TheInkedEngineer <firas@hey.com>
2022-09-20 23:07:26 +0200

    Update release branch

--- Frameworks/TrueLayerUI.xcframework/ios-arm64/TrueLayerUI.framework/Headers/TrueLayerUI-Swift.h
+++ Frameworks/TrueLayerUI.xcframework/ios-arm64/TrueLayerUI.framework/Headers/TrueLayerUI-Swift.h
@@ -216,6 +216,7 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #endif
 
 
+
 @class NSBundle;
 
 SWIFT_CLASS("_TtC11TrueLayerUI20TrueLayerUIResources")
@@ -264,10 +265,13 @@ SWIFT_CLASS("_TtCOO11TrueLayerUI2UI16CountrySelection4View")
 
 
 
+
+
 @interface View (SWIFT_EXTENSION(TrueLayerUI)) <UISearchControllerDelegate>
 - (void)willPresentSearchController:(UISearchController * _Nonnull)searchController;
 - (void)willDismissSearchController:(UISearchController * _Nonnull)searchController;
 @end
+
 
 
 
