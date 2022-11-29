d8ca7319455c6a606aec2f114fefd6d09b2edbd2
Daven Gomes <daven.gomes@truelayer.com>
2022-11-29 17:24:36 +0000

    [PUX-2781] Add TrueLayerEnvironment enum

--- Objective-C Bridge/TrueLayerBridge.swift
+++ Objective-C Bridge/TrueLayerBridge.swift
@@ -7,18 +7,15 @@ public class TrueLayerBridge: NSObject {
   /// Configures and sets up the SDK for a given environment.
   /// - Parameter environment: The environment to set up the SDK with.
   @objc
-  public static func configure(with environment: String) throws {
+  public static func configure(with environment: TrueLayerEnvironment) {
     let trueLayerEnvironment: TrueLayer.Environment
 
-    switch environment.lowercased() {
-      case "sandbox":
+    switch environment {
+      case .sandbox:
         trueLayerEnvironment = .sandbox
-        
-      case "production":
+
+      case .production:
         trueLayerEnvironment = .production
-        
-      default:
-        throw TrueLayerObjectiveCError.invalidEnvironment
     }
 
     TrueLayer.Payments.manager.configure(environment: trueLayerEnvironment)
--- Objective-C Bridge/TrueLayerObjectiveCBridge.h
+++ Objective-C Bridge/TrueLayerObjectiveCBridge.h
@@ -1,14 +1,16 @@
 #ifndef Header_h
 #define Header_h
 
+#import "TrueLayerObjectiveCEnvironment.h"
+
 /// A native bridging class to use the `TrueLayerSDK.TrueLayer.Payments.Manager` in Objective-C projects.
 @interface TrueLayerObjectiveCBridge: NSObject
 /// Configures and sets up the SDK for a given environment.
 /// - Parameters:
 ///   - environment: The environment to set up the SDK with.
 ///   - error: An error that occurs during configuration, for example if the environment is not valid.
-+(void)configureWith:(NSString *_Nonnull)environment
-               error:(NSError * _Nullable * _Nullable)error;
++(void)configureWith:(TrueLayerObjectiveCEnvironment)environment;
+
 @end
 
 #endif /* Header_h */
