48fa7b08f78a9a0e21ecba3c59dab64fbc241664
Daven Gomes <daven.gomes@truelayer.com>
2022-12-05 13:46:03 +0000

    [PUX-2787] Fix init method

--- Objective-C Bridge/Models/Mandate/Context/TrueLayerMandateContext.h
+++ Objective-C Bridge/Models/Mandate/Context/TrueLayerMandateContext.h
@@ -22,7 +22,7 @@
 ///   - resourceToken: The security token associated with the mandate.
 ///   - redirectURL: The destination where the user should be redirected once the authorization flow is done outside of the app (bank website, HPP).
 ///   - preferences: The preferences to apply to the mandate.
-- (nonnull instancetype)initWitIdentifier:(NSString * _Nonnull)identifier
+- (nonnull instancetype)initWithIdentifier:(NSString * _Nonnull)identifier
                             resourceToken:(NSString * _Nonnull)resourceToken
                               redirectURL:(NSURL * _Nonnull)redirectURL
                            preferences:(TrueLayerMandatePreferences * _Nonnull)preferences;
